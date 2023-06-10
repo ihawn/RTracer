eb8d32432c63fdf7e08fbc63526a49a0f42abd6d
Isaac hawn <ihawn.pl15@gmail.comm>
2023-06-10 12:06:16 -0500

    Benchmark changes

--- r_tracer/benches/benches.rs
+++ r_tracer/benches/benches.rs
@@ -1,8 +1,13 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("render_suzanne_after_bvh_update", |b| b.iter(|| r_tracer::render_suzanne(black_box(20))));
+    c.bench_function("render_suzanne_after_bvh_update", |b| {
+        b.iter_batched(
+            || 20,
+            |num_samples| r_tracer::render_suzanne(black_box(num_samples)),
+            criterion::BatchSize::SmallInput,
+        )
+    });
 }
 
 criterion_group!(benches, criterion_benchmark);
